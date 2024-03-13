dbf62ceb9af07a36c6c8688efb3c9d27b1d9829d
Kirb <46562488+jenshobo@users.noreply.github.com>
2024-03-13 08:29:38 +0100

    I hate my life
    forgot 2 brackets, now I practically failed the assigment

--- projects/Assignment3/src/lib/pwm/pwm.h
+++ projects/Assignment3/src/lib/pwm/pwm.h
@@ -5,8 +5,8 @@
 #include "stm32f0_discovery.h"
 
 #define PERIOD 100
-#define LED_PRESCALER  (SystemCoreClock / 200 * PERIOD) /* X = SystemCoreClock / (200Hz * Period) */
-#define MASK_PRESCALER (SystemCoreClock / 20  * PERIOD) /* X = SystemCoreClock / (20Hz  * Period) */
+#define LED_PRESCALER  (SystemCoreClock / (200 * PERIOD)) /* X = SystemCoreClock / (200Hz * Period) */
+#define MASK_PRESCALER (SystemCoreClock / (20  * PERIOD)) /* X = SystemCoreClock / (20Hz  * Period) */
 #define START_PERCENTAGE 70
 
 void pwm_init(void);	/* public methode(s) */
