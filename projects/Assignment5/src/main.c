04fdcfe3543fbb0e23f94bf4136925b23c81b8c7
Kirb <46562488+jenshobo@users.noreply.github.com>
2024-03-19 12:31:28 +0100

    Adding assignment 5
    adding empty assignment 5 project

---
+++ projects/Assignment5/src/main.c
@@ -0,0 +1,10 @@
+/***************************************/
+/* Code writen for Windesheim IOT 2024 */
+/***************************************/
+
+#include "stm32f0xx.h"
+
+int main(void)
+{
+	for(;;) { ; }	/* Idle loop */
+}
